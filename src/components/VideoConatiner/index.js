06183c6ca34093b1cd861e3f10f827f7df85f19a
Zhenyatta <zhenyapaniryan@gmail.com>
2023-11-13 09:10:00 +0400

    change video link

--- src/components/VideoConatiner/index.js
+++ src/components/VideoConatiner/index.js
@@ -7,7 +7,7 @@ const VideoContainer = () => {
             <p className="video-container-text" >Սիրելի՜ Լիանա և Գարիկ, Team2B-ի ողջ թիմը շնորհավորում է Ձեզ նշանադրության կապակցությամբ: <br/> Մաղթում ենք միայն երջանիկ, միայն լուսավոր ու միայն ջերմ օրեր թե՛ միմյանց, թե՛ նաև թիմի հետ միասին: Շա՜տ ժպտացե՜ք, շա՜տ սիրե՜ք ու անսահման երջանիկ եղե՜ք:
             </p>
             <video controls className="video" autoPlay>
-                <source src="//cdn-cf-east.streamable.com/video/mp4/9tig14.mp4?Expires=1700077320&Signature=g~kFNIQ31bb0Ji3mjxZnAtd9AW5uct5uJIyvxAyWLzhlvfqWLw7w1mY6z3amEztMBMuXFLZx9wwkQsPc8DO0aUkiN6bqFtY4HBYoG3KdRBj8yU5ZQWjch70vBFzDfMQx3qL5rRTj1SOT2FYPOTNUL67EMA9zPxF~I8ZdLiw-gKjG5Y3y-HroWkXzCZaz5U57FHGwNwSJ-D1n3Ah27rGwU3ZD8eoaYHVZTM2JhUmkCZ4gWI0J0xvs8vOgb89F-hgcJWWbMhmQWEMJbst~UhBwFQRGZT3DdGMAMhgEQMRdumAYizG8SVYzIWEcVJOri9iEgA3hGZTl6Ge3HmJufddIOg__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ" type="video/mp4" />
+                <source src="//cdn-cf-east.streamable.com/video/mp4/g0ubdw.mp4?Expires=1700111280&Signature=GNt2ZB-p0~PAoWKplszxY~NZGxy6Irxfth79T5Egh5JJtff0-m87Ygr6vRuOdEkHhBYb-5XbcHYvRKDviWhACQIoAIDZpEG1sGl4xF-bgpobofQvAszkmaHq1U41pN~8Vi0cFDaUBWAL-wu6uTphXtlkkFD9nt3vFZW7I40KM2deCa8MlG8~Xynd93U6O5qdKGZ14n11lEqkVb1~BX7cLcVvA~-1nw7E-2xJsq6ZvY6jCRlEM9B9wvJPyuml4HjWVghGZOLDv-qfq8x5ZHK46GW5fkD2fqSOzjrWg5Qn7TQmdAcpob-QQu0EiAvJnqAry5jnHlmBh-Mh8GPPdzOSIg__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ" type="video/mp4" />
                 Sorry, your browser doesn't support videos.
             </video>
             <p className="video-container-p" >What If flowers wishes you all the best !</p>
